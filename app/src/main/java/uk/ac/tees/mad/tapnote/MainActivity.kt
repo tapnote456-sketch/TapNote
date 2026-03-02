90d44d7ecc96522b28110b4578bd72f3bf74c647
Ananthula Akanksh reddy <tapnote456@gmail.com>
2026-03-02 12:00:00 +0530

    Initial commit

---
+++ app/src/main/java/uk/ac/tees/mad/tapnote/MainActivity.kt
@@ -0,0 +1,18 @@
+package uk.ac.tees.mad.tapnote
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import uk.ac.tees.mad.tapnote.ui.theme.TapNoteTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TapNoteTheme {
+            }
+        }
+    }
+}
